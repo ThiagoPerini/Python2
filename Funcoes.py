217a06bdca0a1784e90bc7c649f974e7e37a77d3
Thiago Perini <145079557+ThiagoPerini@users.noreply.github.com>
2024-01-06 23:40:45 -0300

    Update Calculadora v3
    - Adicionado comentários em cada comando explicando o que o mesmo faz.
    
    -Alteração no nome da variável 'operacao' para 'adicionar'

--- Funcoes.py
+++ Funcoes.py
@@ -1,40 +1,40 @@
 
 def calculadora(tipo):
-    if tipo == 'soma':
+    if tipo == 'soma':                   # Condição IF para verificar qual tipo de operação o usuário deseja
         def soma(*args):
-            return sum(args)
-        return soma
+            return sum(args)             # Somar e retornar os valores recebidos pelo *args
+        return soma                      # Retornar o valor da soma
 
     elif tipo == 'subtrair':
         def subtrair(*args):
             valor = 0
-            for item in args:
-              if valor == 0 and item >= 1:
-                  valor = item - valor
+            for item in args:                    # Looping para pegar todos os valores do *args individualmente
+              if valor == 0 and item >= 1:       # Condição de iniciar
+                  valor = item - valor           # Valor recebe item(valor que o usuário digitou) - valor
               else:
-                  valor = valor - item
-            return valor
-        return subtrair
+                  valor = valor - item           # Valor - item(segundo valor que o usuário digitou)
+            return valor                         # Retorna o valor
+        return subtrair                          # Retorna a função
 
     elif tipo == 'multiplicar':
         def multiplicar(*args):
             valor = 1
-            for valores in args:
-                valor = valores * valor
-            return valor
-        return multiplicar
+            for valores in args:                 # Looping para pegar todos os valores em args
+                valor = valores * valor          # Variável valor vai receber cada valor de args * ela mesma.
+            return valor                         # Retorna o valor
+        return multiplicar                       # Retorna a função multiplicar
 
     elif tipo == 'dividir':
         def dividir(*args):
             valor = 0
-            for item in args:
-                if valor == 0 and item > valor:
-                    valor = 1
-                    valor = item / valor
-                else:
-                    valor = valor / item
-            return valor
-        return dividir
+            for item in args:                    # Looping para pegar todos os valores em args
+                if valor == 0 and item > valor:  # Condição para iniciar o programa, irá passar aqui somente uma vez
+                    valor = 1                    # Valor receberá o valor 1 para não apresentar erro de divisão por zero
+                    valor = item / valor         # Valor vai receber o item / valor, no caso. item / 1
+                else:                            # Valor será o número que o usuário digitar por primeiro.
+                    valor = valor / item         # Se não, valor vai receber ele mesmo dividido por item
+            return valor                         # Retorna o valor
+        return dividir                           # Retorna a função dividir
 
 
 
@@ -44,25 +44,36 @@ valor2 = int(input('Informe o segundo valor: '))
 
 
 if tipo_operacao == 'adicionar':
-    operacao = calculadora('soma')
+    adicionar = calculadora('soma')   # Variável adicionar vai receber a função calculadora atribuida com tipo 'soma'
     print('\nVocê escolheu adição')
-    print(f'O resultado da adição de {valor1} + {valor2} = ', operacao(valor1, valor2))
+    print(f'O resultado da adição de {valor1} + {valor2} = ', adicionar(valor1, valor2))
+    # Esse print acima vai imprimir os valores digitados pelo usuário em {valor1} + {valor2}
+    # e vai atribuir a função adicionar os argumentos valor1, valor2 (No caso, os valores digitados lá em cima)
 
 elif tipo_operacao == 'subtrair':
-    subtrair = calculadora('subtrair')
+    subtrair = calculadora('subtrair')  # Variável subtrair vai receber a função calculadora atribuida com tipo 'subtrair'
     print('\nVocê escolheu subtração')
     print(f'O resultado da subtração de {valor1} - {valor2} =  ', subtrair(valor1, valor2))
+    # Esse print acima vai imprimir os valores digitados pelo usuário em {valor1} + {valor2}
+    # e vai atribuir a função subtrair os argumentos valor1, valor2.
+
 
 elif tipo_operacao == 'multiplicar':
-    multiplicar = calculadora('multiplicar')
+    multiplicar = calculadora('multiplicar') # Variável multiplicar vai receber a função calculadora atribuida com tipo 'multiplicar'
     print('\nVocê escolheu multiplicar')
     print(f'O resultado da multiplicação de {valor1} * {valor2} = ', multiplicar(valor1, valor2))
+    # Esse print acima vai imprimir os valores digitados pelo usuário em {valor1} + {valor2}
+    # e vai atribuir a função multiplicar os argumentos valor1, valor2.
+
 
 elif tipo_operacao == 'dividir':
-    try:
-        dividir = calculadora('dividir')
-        dividir(valor1, valor2)
+    try:                                   # Try irá tentar executar esse bloco de comando abaixo
+        dividir = calculadora('dividir')   # Variável dividir vai receber a função calculadora atribuida com tipo 'dividir'
+        dividir(valor1, valor2)      # Atribuindo os valores valor1, valor2 a função dividir
         print('\nVocê escolheu dividir')
         print(f'O resultado da divisão de {valor1} / {valor2} =', dividir(valor1, valor2))
+        # O print acima se tudo funcionar corretamente irá imprimir os valores digitados pelo usuário em {valor1} + {valor2}
+        # e vai atribuir a função dividir os argumentos valor1, valor2.
     except ZeroDivisionError:
-        print('\nErro Divisão por zero')
+        print('\nErro Divisão por zero')  # Se o Try não for executado por conta de divisão de zero
+                                          # irá imprimir uma mensagem ao usuário informando sobre.
